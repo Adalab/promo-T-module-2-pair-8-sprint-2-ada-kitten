2b9ed321fc3b6d763dce9df20521f72a05e0454c
CrisVdev <cristina_cris100@hotmail.com>
2023-05-21 19:20:06 +0200

    2.1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
 'use strict'
 
-document.querySelector(".js-list")
 
-const kittenOne = <li class="card"> 
- <article>
-              <img
-                class="card_img"
-                src="https://dev.adalab.es/gato-siames.webp"
-                alt="siames-cat"
-              />
+const kittenOne = `<li class="card" class="js_siames">
+            <article>
+              <img class="card_img" src="https://dev.adalab.es/gato-siames.webp" alt="siames-cat" />
               <h3 class="card_title">Anastacio</h3>
               <h4 class="card_race">Siamés</h4>
               <p class="card_description">
@@ -17,5 +12,34 @@ const kittenOne = <li class="card">
                 hace 500 años, donde tuvo su origen muy posiblemente.
               </p>
             </article>
+          </li>`;
+const kittenTwo=`<li class="card">
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/sphynx-gato.webp"
+              alt="sphynx-cat"
+            />
+            <h3 class="card_title">Fiona</h3>
+            <h4 class="card_race">Sphynx</h4>
+            <p class="card_description">
+              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
+              hasta con pinta de alienígena han llegado a definir a esta raza
+              gatuna que se caracteriza por la «ausencia» de pelo.
+            </p>
+          </li>`;
+const kittenThree=`<li class="card">
+            <img
+              class="card_img"
+              src="https://dev.adalab.es/maine-coon-cat.webp"
+              alt="maine-coon-cat"
+            />
+            <h3 class="card_title">Cielo</h3>
+            <h4 class="card_race">Maine Coon</h4>
+            <p class="card_description">
+              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+              bella mirada se ha convertido en una de sus señas de identidad.
+              Sus ojos son grandes y las orejas resultan largas y en punta.
+            </p>
+          </li>`;
 
-   
+document.querySelector(".js-list").innerHTML= kittenOne + kittenTwo + kittenThree;
